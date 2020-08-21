b5280720e0dc78cea5802523a29567abaa5d6cdd
Tobbe Lundberg <tobbe@tlundberg.com>
2020-08-21 11:26:15 +0200

    Part 2 done

--- web/src/pages/HomePage/HomePage.js
+++ web/src/pages/HomePage/HomePage.js
@@ -1,7 +1,13 @@
 import BlogLayout from 'src/layouts/BlogLayout'
 
+import BlogPostsCell from 'src/components/BlogPostsCell'
+
 const HomePage = () => {
-  return <BlogLayout>Home</BlogLayout>
+  return (
+    <BlogLayout>
+      <BlogPostsCell />
+    </BlogLayout>
+  )
 }
 
 export default HomePage
